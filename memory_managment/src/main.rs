7c309877aaa9f8061b01025670d55af32522d017
vicky <tyagivivek528@gmail.com>
2023-07-23 04:58:17 +0530

    Learnt about borrowing

--- memory_managment/src/main.rs
+++ memory_managment/src/main.rs
@@ -16,14 +16,21 @@ fn fn2() {
 }
 
 fn ownership() {
-    let s1 = String::from("hello");
-    // let s2 = s1;//Shallow copy, when the value owner is changed to s2 and ownership is taken from s1
+    let mut s1 = String::from("hello");
+    // let s2 = s1;/s/Shallow copy, when the value owner is changed to s2 and ownership is taken from s1
     // let s2 = s1.clone();
     // println!("{},{}", s1, s2);
-    let s2 = ownership_1(s1);
-    println!("{}", s2);
+    let length = ownership_1(&mut s1);
+    println!("The length of {} is {}", s1, length);
+    change_name(&mut s1);
+    println!("{}", s1);
 }
 
-fn ownership_1(s: String) -> String {
-    s
+fn ownership_1(s: &str) -> usize {
+    s.len() //Here to avoid the double free error , or ownership error, because if we directly pass the string then function param will take the ownerhsip of s1 which again throws error, so we have pass the reference to the s1 to the function param ,so even when the function dropped s1 is still presereved
 }
+
+fn change_name(s: &mut String) {
+    s.push_str(" Rust");
+}
+//Mutable Borrowing: When mutable reference is passed, so other variable can manipulate the original value
