78f9d7d3d0544be0c3042e7ea70e80d96444c32c
vicky <tyagivivek528@gmail.com>
2023-08-03 13:32:39 +0530

    Array space occupier method

--- rust_by_Practise/arrays/src/main.rs
+++ rust_by_Practise/arrays/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     println!("Hello Arrays");
     fn1();
+    fn2();
+    fn3();
 }
 
 fn fn1() {
@@ -10,10 +12,24 @@ fn fn1() {
 
 fn fn2() {
     // Fill the blank with proper array type
-    let arr: [u8; 4] = [1, 2, 3, 4];
+    let arr: [_; 4] = [1, 2, 3, 4];
 
     // Modify the code below to make it work
     assert!(arr.len() == 4);
+
+    println!("Success!");
+}
+
+fn fn3() {
+    // We can ignore parts of the array type or even the whole type, let the compiler infer it for us
+    let arr0 = [1, 2, 3];
+    let arr: [_; 3] = ['a', 'b', 'c'];
+
+    // Fill the blank
+    // Arrays are stack allocated, `std::mem::size_of_val` returns the bytes which an array occupies
+    // A char takes 4 bytes in Rust: Unicode char
+    assert!(std::mem::size_of_val(&arr) == 12); //this method prints the space array is taking
+    println!("{}", std::mem::size_of_val(&arr0));
 
     println!("Success!");
 }
