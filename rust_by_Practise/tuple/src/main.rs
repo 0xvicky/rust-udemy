d164c2a77c01f6a8f91b0b6ecd7d3782e190aae9
vicky <tyagivivek528@gmail.com>
2023-08-04 11:47:11 +0530

    Tuple indexing

--- rust_by_Practise/tuple/src/main.rs
+++ rust_by_Practise/tuple/src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     //Tuples are the types which can hold many datatypes in single variable
     println!("Tuples!!");
     fn1();
+    fn2();
 }
 
 fn fn1() {
@@ -10,5 +11,11 @@ fn fn1() {
     let _t1: (u8, (i16, u32)) = (0, (-1, 1));
     // Fill the blanks to make the code work
     let t: (u8, u16, i64, &str, String) = (1u8, 2u16, 3i64, "hello", String::from(", world"));
+    println!("Success!");
+}
+
+fn fn2() {
+    let t = ("i", "am", "sunface");
+    assert_eq!(t.1, "am"); //tuple indexing....
     println!("Success!");
 }
