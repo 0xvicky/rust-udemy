a3f9b7c7e9e56f0a45b874d1ab105399706e8bbb
vicky <tyagivivek528@gmail.com>
2023-08-04 10:56:48 +0530

    usize type decidability

--- rust_by_Practise/slices/src/main.rs
+++ rust_by_Practise/slices/src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     //Slices are the reference to the unknown length of Arrays, Vector, or Strings
     println!("Slices!");
     fn1();
+    fn2();
 }
 
 fn fn1() {
@@ -9,6 +10,17 @@ fn fn1() {
     let s1: &[i32] = &arr[0..2];
 
     let s2: &str = "hello, world" as &str;
+    println!("Success!");
+}
+
+fn fn2() {
+    let arr: [char; 3] = ['中', '国', '人'];
+
+    let slice: &[char] = &arr[..2];
+
+    // Modify '8' to make it work
+    // TIPS: slice( reference ) IS NOT an array, if it is an array, then `assert!` will be passed: Each of the two chars '中' and '国'  occupies 4 bytes, 2 * 4 = 8
+    assert!(std::mem::size_of_val(&slice) == 16); //Here we've used "usize" so the processor architecture decides what to choose for the slice type, so one character use 8 bytes , so both takes 16 bytes
 
     println!("Success!");
 }
