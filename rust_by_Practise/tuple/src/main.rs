f44ae9824e2eba3bcdad4ed26be0d304a49d1225
vicky <tyagivivek528@gmail.com>
2023-08-04 12:07:52 +0530

    tuple can be passed as function arguments

--- rust_by_Practise/tuple/src/main.rs
+++ rust_by_Practise/tuple/src/main.rs
@@ -3,6 +3,9 @@ fn main() {
     println!("Tuples!!");
     fn1();
     fn2();
+    fn3();
+    fn4();
+    tuple_fn_arg();
 }
 
 fn fn1() {
@@ -18,4 +21,36 @@ fn fn2() {
     let t = ("i", "am", "sunface");
     assert_eq!(t.1, "am"); //tuple indexing....
     println!("Success!");
+}
+
+fn fn3() {
+    //Too long tuple cannot printed, we can only print 12 elemented tuple
+    let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+    println!("too long tuple: {:?}", too_long_tuple);
+}
+
+fn fn4() {
+    let tup: (u8, f64, &str) = (1, 6.4, "hello");
+
+    // Fill the blank to make the code work
+    let (x, z, y) = tup;
+
+    assert_eq!(x, 1);
+    assert_eq!(y, "hello");
+    assert_eq!(z, 6.4);
+
+    println!("Success!");
+}
+
+fn tuple_fn_arg() {
+    // Fill the blank, need a few computations here.
+    let (x, y) = sum_multiply((2i32, 3i32));
+
+    assert_eq!(x, 5);
+    assert_eq!(y, 6);
+
+    println!("Success!");
+}
+fn sum_multiply(nums: (i32, i32)) -> (i32, i32) {
+    (nums.0 + nums.1, nums.0 * nums.1)
 }
