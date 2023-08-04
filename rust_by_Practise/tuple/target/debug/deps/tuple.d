cfdecfeb4821d08203df811ce996962f3dc317fd
vicky <tyagivivek528@gmail.com>
2023-08-04 11:45:50 +0530

    started to learn about tuples

---
+++ rust_by_Practise/tuple/target/debug/deps/tuple.d
@@ -0,0 +1,5 @@
+E:\Code\Rust\Rust Udemy\rust_by_Practise\tuple\target\debug\deps\tuple.exe: src\main.rs
+
+E:\Code\Rust\Rust Udemy\rust_by_Practise\tuple\target\debug\deps\tuple.d: src\main.rs
+
+src\main.rs:
---
+++ rust_by_Practise/tuple/target/debug/tuple.d
@@ -0,0 +1,1 @@
+E:\Code\Rust\Rust\ Udemy\rust_by_Practise\tuple\target\debug\tuple.exe: E:\Code\Rust\Rust\ Udemy\rust_by_Practise\tuple\src\main.rs
